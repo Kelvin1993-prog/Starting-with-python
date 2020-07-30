042faf354d564523d0478d088f49a9ad2c1455e1
kelvin <nnabugwukelvin1993@gmail.com>
2020-07-30 11:52:36 +0100

    Add files via upload

---
+++ Starting_with_python.py
@@ -0,0 +1,142 @@
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "metadata": {},
+   "source": [
+    "### All the things you need to know to start with Python"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "hello world\n"
+     ]
+    }
+   ],
+   "source": [
+    "#let's start with the first every concept in programming language\n",
+    "#by now, you should have python3 installed in your system\n",
+    "\n",
+    "print(\"hello world\")\n",
+    "\n",
+    "#a traditional belief in programming world, that starting with this brings luck. So goodluck to you"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "metadata": {},
+   "source": [
+    "congrats, you're now a programmer, so where do we move on next?\n",
+    "\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 4,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "hello python world\n"
+     ]
+    }
+   ],
+   "source": [
+    "#next step? let's get familiar with variables and data types\n",
+    "\n",
+    "#variables\n",
+    "word = \"hello python world\"\n",
+    "print(word)"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "metadata": {},
+   "source": [
+    "the same output like before right? this is so because we have added a variable name called word.\n",
+    "\n",
+    "so what do they do? let's imagine a room filled with chocolates. The room serves as a store for chocolates and chocolates in this sense serves as a value.\n",
+    "\n",
+    "so from our previous task, the variable,(room) is \"word\", while the value is \"hello python world\""
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 5,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "hello python, best place to live in the world\n"
+     ]
+    }
+   ],
+   "source": [
+    "#let us repeat this step for better clarification\n",
+    "word = \"hello python, best place to live in the world\"\n",
+    "print(word)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 6,
+   "metadata": {},
+   "outputs": [
+    {
+     "ename": "NameError",
+     "evalue": "name 'wod' is not defined",
+     "output_type": "error",
+     "traceback": [
+      "\u001b[1;31m---------------------------------------------------------------------------\u001b[0m",
+      "\u001b[1;31mNameError\u001b[0m                                 Traceback (most recent call last)",
+      "\u001b[1;32m<ipython-input-6-d57874295f4f>\u001b[0m in \u001b[0;36m<module>\u001b[1;34m\u001b[0m\n\u001b[1;32m----> 1\u001b[1;33m \u001b[0mprint\u001b[0m\u001b[1;33m(\u001b[0m\u001b[0mwod\u001b[0m\u001b[1;33m)\u001b[0m\u001b[1;33m\u001b[0m\u001b[1;33m\u001b[0m\u001b[0m\n\u001b[0m",
+      "\u001b[1;31mNameError\u001b[0m: name 'wod' is not defined"
+     ]
+    }
+   ],
+   "source": [
+    "#string data type\n",
+    "\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.7"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
